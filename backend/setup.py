6443153d37b3376271714a9057caf928b13a1118
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-08-04 14:31:59 -0700

    Upgrade Python packages
    - Django 1.11.3 => 1.11.4
    - ARCUtils 2.22.0 => 2.23.0
    - psycopg2 2.7.1 => 2.7.3

--- backend/setup.py
+++ backend/setup.py
@@ -12,13 +12,13 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django>=1.11.3',
-        'django-arcutils>=2.22.0',
+        'django>=1.11.4',
+        'django-arcutils>=2.23.0',
         'django-local-settings>=1.0b7',
         'django-pgcli>=0.0.2',
         'djangorestframework>=3.6.3',
         'Markdown>=2.6.8',
-        'psycopg2>=2.7.1',
+        'psycopg2>=2.7.3',
         'psu.oit.arc.tasks',
     ],
     extras_require={
